4c8c2e4fc897e5f860199290774ef857b0c909e0
yusu <dbtnss@naver.com>
2024-02-08 10:20:38 +0900

    Docker Compose 작성 및 실행

--- docker-compose-app/server.js
+++ docker-compose-app/server.js
@@ -2,20 +2,44 @@ const express = require('express');
 const redis = require('redis');
 
 const client = redis.createClient({
-  host: 'redis-server',
-  port: 6379
+  url: 'redis://redis-server:6379'
 })
 
+const connect = async () => {
+  if (!client.isOpen) {
+    await client.connect()
+    await client.set('number', 0);
+
+  }
+}
+
+const disconnect = async () => {
+  if (client.isOpen) {
+    await client.disconnect();
+  }
+}
+
 const app = express();
+connect();
 
-client.set('number', 0);
+app.get('/', async (req, res) => {
+  await connect();
 
-app.get('/', (req, res) => {
-  client('number', (err, number) => {
-    client.set('number', parseInt(number) + 1);
-    res.send('숫자가 1씩 올라갑니다. 숫자: ' + number);
-  });
+  let number = await client.get('number');
+  if (number === null) {
+    number = 0;
+  }
+
+  res.send('숫자가 1씩 올라갑니다. 숫자: ' + number);
+
+  await client.set('number', parseInt(number) + 1);
+});
+
+app.get('/disconnect', async (req, res) => {
+  await disconnect();
+  res.send('연결이 종료되었습니다.');
 });
 
 app.listen(8080);
+
 console.log('Server is running');
